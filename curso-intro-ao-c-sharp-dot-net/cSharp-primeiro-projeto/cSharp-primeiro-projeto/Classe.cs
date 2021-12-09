aa9ac1aa328361c3a799feb19500deabfccfb68e
Renata Santos <renatados.santosp@gmail.com>
2021-12-08 22:33:07 -0300

    namespace e classes

---
+++ curso-intro-ao-c-sharp-dot-net/cSharp-primeiro-projeto/cSharp-primeiro-projeto/Classe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_primeiro_projeto
+{
+    class Pessoa
+    {
+        // métodos
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public int Idade { get; set; }
+    }
+}
---
+++ curso-intro-ao-c-sharp-dot-net/cSharp-primeiro-projeto/cSharp-primeiro-projeto/Program.cs
@@ -0,0 +1,29 @@
+﻿using.System;
+using cSharp_primeiro_projeto;
+
+namespace cSharp_primeiro_projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa person = new Pessoa();
+
+            person.Nome = "Renata";
+            person.Apelido = "Re";
+            person.Idade = 25;
+
+            //aqui eu to criando mais objetos para a variável (person2) dentro da classe Pessoa()
+            var person2 = new Pessoa();
+
+            person2.Nome = "Juliana";
+            person2.Apelido = "Ju";
+            person2.Idade = 26;
+
+            var classe = new Classe();
+            var classe2 = new Segundo.Classe();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
