0984e7c27b42ca51330d8440675c97b93a03d014
Renata Santos <renatados.santosp@gmail.com>
2021-12-11 22:23:50 -0300

    update estudo

---
+++ curso-intro-ao-c-sharp-dot-net/hello-world/hello-world/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Olá, Mundo!");
+            Console.ReadLine();
+
+        }
+    }
+}
