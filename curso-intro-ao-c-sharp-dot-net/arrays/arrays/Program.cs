ae3c231a0396fbc9130748a80df63420857c146a
Renata Santos <renatados.santosp@gmail.com>
2021-12-15 23:38:59 -0300

    update

---
+++ curso-intro-ao-c-sharp-dot-net/arrays/arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //criar um array de aves da mata atlântica fluminense
+            string[] avesMataAtlantica = new string[3] {
+                "tiê-sangue", //0
+                "lavadeira-mascarada", //1
+                "garça-branca-grande", //2
+            };
+
+            avesMataAtlantica[1] = "maria-faceira";
+
+            Console.WriteLine(avesMataAtlantica[1]);
+            Console.ReadLine();
+
+        }
+    }
+}
