891431ba10a454fdc42935fe85f0241060246de5
Renata Santos <renatados.santosp@gmail.com>
2022-01-09 18:31:11 -0300

    matrizes

--- ExemploColecoes/Colecoes/Program.cs
+++ ExemploColecoes/Colecoes/Program.cs
@@ -12,20 +12,41 @@ namespace Colecoes
 
             arrayInteiros[0] = 12;
             arrayInteiros[1] = 22;
-            arrayInteiros[2] = 32;
+            arrayInteiros[2] = int.Parse("32");
 
             //Duas formas de buscar esses dados
+            Console.WriteLine("Percorrendo a lista pelo For");
+            
+            for (int n = 0;  n < arrayInteiros.Length; n++)
+            {
+                System.Console.WriteLine(arrayInteiros[n]);
+            }
+
+            Console.WriteLine("Percorrendo a lista pelo Foreach");
+            
             foreach (int n in arrayInteiros)
             {
                 Console.WriteLine(n);
             }
 
-            Console.WriteLine();
+            Console.WriteLine("Percorrendo a matriz:");
 
-            for (int n = 0;  n < arrayInteiros.Length; n++)
+            int[,] matriz = new int[4, 2]
             {
-                System.Console.WriteLine(arrayInteiros[n]);
+                { 8, 8 },
+                { 10, 20 },
+                { 13, 4 },
+                { 12, 3}
+            };
+
+            for (int n = 0; n < matriz.GetLength(0); n++)
+            {
+                for (int i = 0; i < matriz.GetLength(1); i++)
+                {
+                    Console.WriteLine(matriz[n, i]);
+                }
             }
+
         } 
     }
 }
